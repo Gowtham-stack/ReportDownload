ecd29725825600625398342fe065b184d3547bc6
Gowtham Raj <Gowthamraj.engg@gmail.com>
2022-08-22 14:46:28 +0530

    Add project files.

---
+++ Controllers/ReportDownloadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportDownload.Processors;
+
+namespace ReportDownload.Controllers
+{
+    public class ReportDownloadController : Controller
+    {
+        private readonly ReportProcessInServer reportProcessInServer;
+
+        public ReportDownloadController(ReportProcessInServer reportProcessInServer)
+        {
+            this.reportProcessInServer = reportProcessInServer;
+        }
+
+        [HttpGet("GenerateReport")]
+        public async Task<IActionResult> GenerateReportAsync(CancellationToken cancellationToken)
+        {
+            await reportProcessInServer.GenerateReport(cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ Processors/ReportProcessInServer.cs
@@ -0,0 +1,12 @@
+﻿namespace ReportDownload.Processors
+{
+    public class ReportProcessInServer
+    {
+        public async Task GenerateReport(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Report Started Generating");
+            await Task.Delay(TimeSpan.FromSeconds(20), cancellationToken);
+            Console.WriteLine("Report Completed Generating");
+        }
+    }
+}
